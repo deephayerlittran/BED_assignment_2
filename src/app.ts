5243a4d054c7e2b2e817afce0e0ca5ff96edca73
Rajandeep20 <Rajandeeplittran@gmail.com>
2025-11-14 04:23:33 -0600

    connected v1 routes to main express app

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,9 @@
 import express from "express";
 import morgan from "morgan";
-import routes from "./api/v1/routes";
+import routes from "../src/api/v1/routes";
 
 const app = express();
+
 app.use(express.json());
 app.use(morgan("combined"));
 
@@ -10,6 +11,7 @@ app.get("/health", (req, res) => {
   res.status(200).send("Server is healthy");
 });
 
+// Connect API routes
 app.use("/api/v1", routes);
 
 export default app;
