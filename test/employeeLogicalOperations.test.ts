23b10bcb4c90dbe31f96f64418aba8fec7270a95
Rajandeep20 <Rajandeeplittran@gmail.com>
2025-11-14 04:41:45 -0600

    fixed the import for logical operations

--- test/employeeLogicalOperations.test.ts
+++ test/employeeLogicalOperations.test.ts
@@ -1,5 +1,5 @@
 import request from "supertest";
-import app from "../app";
+import app from "../src/app";
 
 describe("Employee Logical Operations", () => {
   it("should return employees for a specific branch", async () => {
