6ec15874c5ae72431844f4324c05380dc4b5e9db
Rajandeep20 <Rajandeeplittran@gmail.com>
2025-11-15 11:31:29 -0600

    updated validation

--- src/api/v1/validation/branchValidation.ts
+++ src/api/v1/validation/branchValidation.ts
@@ -3,11 +3,11 @@ import Joi from "joi";
 export const createBranchSchema = Joi.object({
     name: Joi.string().min(2).required(),
     address: Joi.string().min(5).required(),
-    phone: Joi.string().min(10).required(),
+    phone: Joi.string().min(7).required(),
 });
 
 export const updateBranchSchema = Joi.object({
     name: Joi.string().min(2),
     address: Joi.string().min(5),
-    phone: Joi.string().min(10),
-}).min(1);
+    phone: Joi.string().min(7),
+});
--- src/api/v1/validation/employeeValidation.ts
+++ src/api/v1/validation/employeeValidation.ts
@@ -2,18 +2,14 @@ import Joi from "joi";
 
 export const createEmployeeSchema = Joi.object({
     name: Joi.string().min(2).required(),
-    position: Joi.string().min(2).required(),
-    department: Joi.string().min(2).required(),
     email: Joi.string().email().required(),
-    phone: Joi.string().min(10).required(),
-    branchId: Joi.number().required(),
+    position: Joi.string().min(2).required(),
+    branchId: Joi.string().required(),
 });
 
 export const updateEmployeeSchema = Joi.object({
     name: Joi.string().min(2),
-    position: Joi.string().min(2),
-    department: Joi.string().min(2),
     email: Joi.string().email(),
-    phone: Joi.string().min(10),
-    branchId: Joi.number(),
-}).min(1);
+    position: Joi.string().min(2),
+    branchId: Joi.string(),
+});
