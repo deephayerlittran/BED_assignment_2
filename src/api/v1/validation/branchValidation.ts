d4f0040c9861ccbcdd35b54fc8d5e1efd1cd9087
Rajandeep20 <Rajandeeplittran@gmail.com>
2025-11-15 10:28:55 -0600

    added joi validation for branch routes

---
+++ src/api/v1/validation/branchValidation.ts
@@ -0,0 +1,13 @@
+import Joi from "joi";
+
+export const createBranchSchema = Joi.object({
+    name: Joi.string().min(2).required(),
+    address: Joi.string().min(5).required(),
+    phone: Joi.string().min(10).required(),
+});
+
+export const updateBranchSchema = Joi.object({
+    name: Joi.string().min(2),
+    address: Joi.string().min(5),
+    phone: Joi.string().min(10),
+}).min(1);
